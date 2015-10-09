578c763442706f9e90ee8e5885c0243eb360aaae
Pelle Beckman <pelle.beckman@gmail.com>
2015-10-09 16:36:53 +0200

    Initial Commit

---
+++ Mmry/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Mmry
+//
+//  Created by Pelle Beckman on 2015-10-09.
+//  Copyright © 2015 Beckman Creative. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
